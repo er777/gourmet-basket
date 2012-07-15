7dddb02ceeea1462953549d0ce96d17ad105e664
Eddie Reyes <er777@reyesworld.com>
2012-07-15 10:32:09 -0700

    For cat summaries and articles

--- admin/category_editor.php
+++ admin/category_editor.php
@@ -6,14 +6,18 @@ $msg = "";
 $post_type = "add";
 $cid = "";
 $category_name = "";
+$category_summary = "";
 $category_article = "";
+$category_pics = array();
+$msg = '';
+
 if(isset($_POST["post_type"])) {
     switch($_POST["post_type"]){
         case "add":
             if($_POST["category_name"] !=""){
                 DB::connect();
                 $qry = "INSERT INTO categories
-                    SET category_name = '" . $_POST["category_name"] . "' , category_article = '" . $_POST["category_article"] . "' ";
+                    SET category_name = '" . $_POST["category_name"] . "' , category_summary = '" . $_POST["category_summary"] . "' , category_article = '" . $_POST["category_article"] . "' ";
                 DB::execute($qry);
                 DB::close();
                 //header("location: categories.php");
@@ -28,7 +32,7 @@ if(isset($_POST["post_type"])) {
                 DB::connect();
                 $qry = "
                     UPDATE categories
-					SET category_name = '" . $_POST["category_name"] . "' , category_article = '" . $_POST["category_article"] . "'
+					SET category_name = '" . $_POST["category_name"] . "' , category_summary = '" . $_POST["category_summary"] . "' , category_article = '" . $_POST["category_article"] . "' 
                     WHERE category_id = " . $_POST["cid"];"' ";
                 DB::execute($qry);
                 DB::close();
@@ -48,7 +52,9 @@ if(isset($_GET["cmd"]) && $_GET["cmd"]=="edit") {
     DB::query($qry);
     while($row = DB::fetch_row()){
         $category_name = $row["category_name"];
+		$category_summary = $row["category_summary"];
         $category_article = $row['category_article'];
+		
     }
     $post_type = "update";
     DB::close();
@@ -71,40 +77,135 @@ if(isset($_GET["cmd"]) && $_GET["cmd"]=="delete") {
     <form method="post" action="category_editor.php" name="frmAdd">
         <input type="hidden" name="post_type" value="<?php echo $post_type; ?>" />
         <input type="hidden" name="cid" value="<?php echo $cid; ?>" />
-        <table border="0" cellpadding="4" cellspacing="2">
-            <tr>
-                <td width="690"><img src="images/titles/title_<?php echo $post_type; ?>_category.jpg" /></td>
-            </tr>
-            <tr>
-                <td>
-                    Category Name:<br>
-                    <input type="text" name="category_name" id="category_name" size="32" maxlength="32" value="<?php echo $category_name; ?>" /><br>
-                    Category Article:<br>
-                    <div>
-                        <textarea name="category_article" id="categories_categoryArticle" style="width:600px;height:500px;" /> <?=$category_article; ?> </textarea>
-                    </div>
-                    <script src='ckeditor/ckeditor.js' type="text/javascript"></script>
-                    <script type="text/javascript">
-                        CKEDITOR.replace( 'categories_categoryArticle' );
-                    </script>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="image" src="images/btn_<?php echo $post_type; ?>.jpg" onclick="return SaveCategory()" />&nbsp;
-                    <?php
-                    if($post_type=="update") {
-                        ?>
-                        <a href="category_editor.php?cmd=delete&cid=<?php echo $cid; ?>"><img src="images/btn_delete.jpg" border="0" /></a>&nbsp;
-                <a href="subcategories.php?cid=<?php echo $cid; ?>&category_name=<?php echo $category_name; ?>">
-                <?php
-                    }
-                    ?>
-                </td>
-            </tr>
-        </table>
+
+<div id="tabs">
+    <ul>
+        <li><a href="#tabs-1">Category Data</a></li>
+        <li><a href="#tabs-2">Category Images</a></li>
+        
+    </ul>
+            
+		<div id="tabs-1">
+            <?php require_once('utils/categories/category-data.php'); ?>
+            	<div class="clear"></div>
+		</div>
+          
+		<div id="tabs-2">
+			<?php require_once('utils/categories/category-images.php'); ?>
+                 <div class="clear"></div>
+  
+		</div>
+
+        
     </form>
 
 
 </div>
 
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
+<script type="text/javascript" src="js/jquery.bgiframe-2.1.2.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/i18n/jquery-ui-i18n.min.js"></script>
+<script type="text/javascript" src="js/uploadify/swfobject.js"></script>
+<script type="text/javascript" src="js/uploadify/jquery.uploadify.v2.1.4.min.js"></script>
+<script type="text/javascript" language="javascript">
+    $(function() {
+        $("#tabs").tabs();
+		 $("#tabs-cat").tabs();
+		 
+        // Category Images
+      
+        $('#fileimage1').uploadify({
+            'uploader'  : 'js/uploadify/uploadify.swf',
+            'script'    : 'js/uploadify/uploadify.php',
+            'cancelImg' : 'js/uploadify/cancel.png',
+            'buttonText': 'Browse for Image 1',
+			'width'     : 300,
+            'auto'      : true,
+            'folder'    : '../app/webroot/img/categories',
+			'sizeLimit'   : 150000,
+            'onComplete': function(event, queueID, fileObj, response, data) {
+                $('#image1').val(response);
+                $('#bd-imgimage1').attr('src','/img/categories/'+response);
+            }
+        });
+        $('#fileimage2').uploadify({
+            'uploader'  : 'js/uploadify/uploadify.swf',
+            'script'    : 'js/uploadify/uploadify.php',
+            'cancelImg' : 'js/uploadify/cancel.png',
+            'buttonText': 'Browse for Image 2',
+			'width'     : 300,
+            'auto'      : true,
+            'folder'    : '../app/webroot/img/categories',
+			'sizeLimit'   : 150000,
+            'onComplete': function(event, queueID, fileObj, response, data) {
+                $('#image2').val(response);
+                $('#bd-imgimage2').attr('src','/img/categories/'+response);
+            }
+        });
+		 $('#fileimage3').uploadify({
+            'uploader'  : 'js/uploadify/uploadify.swf',
+            'script'    : 'js/uploadify/uploadify.php',
+            'cancelImg' : 'js/uploadify/cancel.png',
+            'buttonText': 'Browse for Image 3',
+			'width'     : 300,
+            'auto'      : true,
+            'folder'    : '../app/webroot/img/categories',
+			'sizeLimit'   : 150000,
+            'onComplete': function(event, queueID, fileObj, response, data) {
+                $('#image3').val(response);
+                $('#bd-imgimage3').attr('src','/img/categories/'+response);
+            }
+        });
+		
+		 $('#fileimage4').uploadify({
+            'uploader'  : 'js/uploadify/uploadify.swf',
+            'script'    : 'js/uploadify/uploadify.php',
+            'cancelImg' : 'js/uploadify/cancel.png',
+            'buttonText': 'Browse for Image 4',
+			'width'     : 300,
+            'auto'      : true,
+            'folder'    : '../app/webroot/img/categories',
+			'sizeLimit'   : 150000,
+            'onComplete': function(event, queueID, fileObj, response, data) {
+                $('#image4').val(response);
+                $('#bd-imgimage4').attr('src','/img/categories/'+response);
+            }
+        });
+		
+		 $('#fileimage5').uploadify({
+            'uploader'  : 'js/uploadify/uploadify.swf',
+            'script'    : 'js/uploadify/uploadify.php',
+            'cancelImg' : 'js/uploadify/cancel.png',
+            'buttonText': 'Browse for Image 5',
+			'width'     : 300,
+            'auto'      : true,
+            'folder'    : '../app/webroot/img/categories',
+			'sizeLimit'   : 150000,
+            'onComplete': function(event, queueID, fileObj, response, data) {
+                $('#image5').val(response);
+                $('#bd-imgimage5').attr('src','/img/categories/'+response);
+            }
+        });
+		
+		 $('#fileimage6').uploadify({
+            'uploader'  : 'js/uploadify/uploadify.swf',
+            'script'    : 'js/uploadify/uploadify.php',
+            'cancelImg' : 'js/uploadify/cancel.png',
+            'buttonText': 'Browse for Image 6',
+			'width'     : 300,
+            'auto'      : true,
+            'folder'    : '../app/webroot/img/categories',
+			'sizeLimit'   : 150000,
+            'onComplete': function(event, queueID, fileObj, response, data) {
+                $('#image6').val(response);
+                $('#bd-imgimage6').attr('src','/img/categories/'+response);
+            }
+        });
+		
+
+		
+	});
+	
+
+</script>
